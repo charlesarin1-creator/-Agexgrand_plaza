ddd552881333af28731a24b3925f19163f4e4b0a
charlesarin1-creator <charlesarin1@gmail.com>
2026-02-14 13:49:10 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -2,6 +2,6 @@
 // Connects your frontend to Supabase (safe: uses publishable key)
 
 const SUPABASE_URL = "https://hfgtijzqrlkivsgwgnbp.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_IGXxIQRWulcE5N0CKqTxyQ_X5FFj194";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhmZ3RpanpxcmxraXZzZ3dnbmJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEwMDcyMDIsImV4cCI6MjA4NjU4MzIwMn0.2HlBUjJXTJwBqGw0UNIBdg42qBuRhZCCThlYcbvtyjQ";
 
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
